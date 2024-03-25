66c6efac03af01d5f49edabb41374a05a004df1b
kiss me! <116779602+kissmepetunia@users.noreply.github.com>
2024-03-25 15:33:16 +0800

    Update detect_torch.py

--- detect_torch.py
+++ detect_torch.py
@@ -107,7 +107,7 @@ def main():
                 cv2.imwrite(os.path.join(data_save, image), img_resize)
                 mark.append(land_resize)
                 np.array(mark,dtype=object)
-                if land_marks.size == 0:
+                if i == 0:
                     land_marks = mark
                 else:
                     land_marks = np.vstack((land_marks, mark))
